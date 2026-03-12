8619aab229b414d708a7ca766311c170c4b2b582
bojidaryovchev <blizzardcon10@gmail.com>
2026-03-13 00:58:47 +0200

    refactor: infra improvements

--- apps/realtime/src/namespaces/presence.ts
+++ apps/realtime/src/namespaces/presence.ts
@@ -4,15 +4,20 @@ import type { Server } from "socket.io";
 export function registerPresenceNamespace(io: Server, log: FastifyBaseLogger) {
   const nsp = io.of("/presence");
 
-  nsp.on("connection", (socket) => {
+  nsp.on("connection", async (socket) => {
     const userId = socket.data.userId as string;
     log.info(`[/presence] connected: ${socket.id} (user: ${userId})`);
 
     // Join user's personal presence room
     socket.join(`user:${userId}`);
 
-    // Broadcast online status
-    nsp.emit("user:online", { userId });
+    // Send the list of currently online users to the newly connected client
+    const sockets = await nsp.fetchSockets();
+    const onlineUserIds = [...new Set(sockets.map((s) => s.data.userId as string))];
+    socket.emit("presence:sync", { userIds: onlineUserIds });
+
+    // Broadcast online status to everyone else
+    socket.broadcast.emit("user:online", { userId });
 
     socket.on("disconnect", async () => {
       log.info(`[/presence] disconnected: ${socket.id} (user: ${userId})`);
